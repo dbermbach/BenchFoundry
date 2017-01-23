339269b9da8a946892d3ddd424baa18a46256cf9
j.kuhlenkamp <j.kuhlenkamp@tu-berlin.de>
2017-01-23 14:22:39 +0100

    Removed external input dependencies from unit test.

--- src/test/java/de/tuberlin/ise/benchfoundry/util/TraceFileSplitterTest.java
+++ src/test/java/de/tuberlin/ise/benchfoundry/util/TraceFileSplitterTest.java
@@ -2,12 +2,7 @@ package de.tuberlin.ise.benchfoundry.util;
 
 import junit.framework.TestCase;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +12,26 @@ import de.tuberlin.ise.benchfoundry.util.TraceFileSplitter;
  * @author Akon Dey (akon.dey@sydney.edu.au)
  */
 public class TraceFileSplitterTest extends TestCase {
+
+	private static final String trace = "BOP;0;4211\n"+
+			"BOT;0\n"+
+			"9;4211\n"+
+			"10;4212\n"+
+			"11;4213\n"+
+			"12;4214\n"+
+			"5;4215\n"+
+			"EOT\n"+
+			"EOP\n"+
+			"\n"+
+			"BOP;0;4212\n"+
+			"BOT;0\n"+
+			"11;4216\n"+
+			"13;4217\n"+
+			"14;4218\n"+
+			"14;4219\n"+
+			"EOT\n"+
+			"EOP";
+
 	private static int NUM_SPLITS = 3;
 	/*
 	 * (non-Javadoc)
@@ -43,18 +58,20 @@ public class TraceFileSplitterTest extends TestCase {
 	 */
 	public void testSprayTraceInputStreamToOutputStreams() {
 		try {
-			InputStream is = new FileInputStream("samples/trace_example.txt");
+			// InputStream is = new FileInputStream("samples/trace_example.txt");
+			// InputStream is = new BufferedInputStream(new FileInputStream("tpcc/tpcc_run"));
+			InputStream is = new StringBufferInputStream(trace);
 			List<OutputStream> os = new ArrayList<OutputStream>(NUM_SPLITS);
 			for (int i = 0; i < NUM_SPLITS; i++) {
 				os.add(new FileOutputStream("/tmp/__SprayTraceOutput__"+ i + ".txt"));
 			}
 			TraceFileSplitter.sprayTraceInputStreamToOutputStreams(is, os);
 		} catch (FileNotFoundException e) {
-			fail("Test failed with exception");
 			e.printStackTrace();
+			fail("Test failed with exception");
 		} catch (IOException e) {
-			fail("Test failed with exception");
 			e.printStackTrace();
+			fail("Test failed with exception");
 		} 
 	}
 }
